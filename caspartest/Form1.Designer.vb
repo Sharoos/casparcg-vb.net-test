323858a787be4f31b5b7021b2c6d6f1de57f4af7
sharath <sharath@SHARATHNANDAN>
2024-03-06 20:31:51 +0530

    060324

--- caspartest/Form1.Designer.vb
+++ caspartest/Form1.Designer.vb
@@ -22,12 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.o = New System.Windows.Forms.OpenFileDialog()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.seekTimer = New System.Windows.Forms.Timer(Me.components)
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Cmdshowcasparcgwindow = New System.Windows.Forms.Button()
+        Me.Cmdoutcasparcgwindow = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
         'Button1
@@ -64,19 +71,70 @@ Partial Class Form1
         Me.Button3.Text = "Stop"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 67)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(237, 195)
+        Me.TextBox2.TabIndex = 5
+        '
+        'seekTimer
+        '
+        Me.seekTimer.Enabled = True
+        '
         'Panel1
         '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel1.Location = New System.Drawing.Point(255, 12)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(400, 250)
-        Me.Panel1.TabIndex = 4
+        Me.Panel1.Size = New System.Drawing.Size(395, 250)
+        Me.Panel1.TabIndex = 8
+        '
+        'Cmdshowcasparcgwindow
+        '
+        Me.Cmdshowcasparcgwindow.Location = New System.Drawing.Point(255, 268)
+        Me.Cmdshowcasparcgwindow.Name = "Cmdshowcasparcgwindow"
+        Me.Cmdshowcasparcgwindow.Size = New System.Drawing.Size(75, 23)
+        Me.Cmdshowcasparcgwindow.TabIndex = 0
+        Me.Cmdshowcasparcgwindow.Text = "In"
+        Me.Cmdshowcasparcgwindow.UseVisualStyleBackColor = True
+        '
+        'Cmdoutcasparcgwindow
+        '
+        Me.Cmdoutcasparcgwindow.Location = New System.Drawing.Point(336, 268)
+        Me.Cmdoutcasparcgwindow.Name = "Cmdoutcasparcgwindow"
+        Me.Cmdoutcasparcgwindow.Size = New System.Drawing.Size(75, 23)
+        Me.Cmdoutcasparcgwindow.TabIndex = 9
+        Me.Cmdoutcasparcgwindow.Text = "Out"
+        Me.Cmdoutcasparcgwindow.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(250, 11)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(411, 27)
+        Me.Panel2.TabIndex = 9
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Location = New System.Drawing.Point(790, 12)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(395, 250)
+        Me.Panel3.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(662, 272)
+        Me.ClientSize = New System.Drawing.Size(1242, 302)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Cmdoutcasparcgwindow)
+        Me.Controls.Add(Me.Cmdshowcasparcgwindow)
         Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
@@ -92,5 +150,11 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents seekTimer As Timer
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents Cmdshowcasparcgwindow As Button
+    Friend WithEvents Cmdoutcasparcgwindow As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
 End Class
--- caspartest/Form1.vb
+++ caspartest/Form1.vb
@@ -1,9 +1,22 @@
 ﻿Imports Svt.Caspar
+Imports System
+Imports System.Diagnostics
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms
 
 Public Class Form1
+
+    Private parentedProcess As Process
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+
     Dim aa As New CasparDevice
     Private g_int_ChannelNumber As Integer = 1
     Private g_int_PlaylistLayer As Integer = 1
+    Private mediaDuration As Integer
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
@@ -14,11 +27,61 @@ Public Class Form1
         If o.ShowDialog = DialogResult.OK Then
             TextBox1.Text = Replace(Replace(o.FileName, ":", ":\"), "\", "/")
         End If
-        aa.SendString("Play " & g_int_ChannelNumber & "-" & g_int_PlaylistLayer & " " & """" & TextBox1.Text & """")
+        aa.SendString($"Play {g_int_ChannelNumber}-{g_int_PlaylistLayer} ""{TextBox1.Text}""")
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         aa.SendString("stop 1-1")
     End Sub
-End Class
 
+    Private Sub Cmdshowcasparcgwindow_Click(sender As Object, e As EventArgs) Handles Cmdshowcasparcgwindow.Click
+        Try
+            Dim p1() As Process = Process.GetProcessesByName("casparcg")
+            Dim iprocess As Integer
+            For iprocess = 0 To p1.GetUpperBound(0)
+                If p1(iprocess).MainWindowTitle = "Screen consumer [1|1080p5000]" Then
+                    Exit For
+                End If
+            Next iprocess
+
+            parentedProcess = p1(iprocess)
+            SetParent(parentedProcess.MainWindowHandle, Panel1.Handle)
+            SendMessage(parentedProcess.MainWindowHandle, 274, 61488, 0)
+
+            ' Embed the "Console Window Host" into Panel2
+            Dim consoleProcesses() As Process = Process.GetProcessesByName("Console Window Host")
+            If consoleProcesses.Length > 0 Then
+                SetParent(consoleProcesses(0).MainWindowHandle, Panel2.Handle)
+                SendMessage(consoleProcesses(0).MainWindowHandle, 274, 61488, 0)
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
+
+    <DllImport("user32.dll")>
+    Private Shared Function SetParent(hWndChild As IntPtr, hWndNewParent As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function SendMessage(hWnd As IntPtr, Msg As Integer, wParam As Integer, lParam As Integer) As IntPtr
+    End Function
+
+    Private Sub Cmdoutcasparcgwindow_Click(sender As Object, e As EventArgs) Handles Cmdoutcasparcgwindow.Click
+        Try
+            SetParent(parentedProcess.MainWindowHandle, IntPtr.Zero)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Try
+            SetParent(parentedProcess.MainWindowHandle, IntPtr.Zero)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
